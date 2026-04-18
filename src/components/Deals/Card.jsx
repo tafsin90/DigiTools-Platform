994b54818caa0f91b48c9ecd43b31147cc868d86
tafsin90 <tafsin2003@gmail.com>
2026-04-18 12:10:28 +0600

    demo-card

--- src/components/Deals/Card.jsx
+++ src/components/Deals/Card.jsx
@@ -1,38 +1,45 @@
-import React from "react";
+import React, { use } from "react";
 import portfolio from "/icons/portfolio.png";
-import { FaCheck } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa6";
 
-function Card() {
+function Card({ dataPromise }) {
+  const datas = use(dataPromise);
+  console.log(datas);
+  
   return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-sm">
-        <div className="card-body">
-          <p className="badge badge-xs badge-warning">best seller</p>
-          <img src={portfolio} className="w-5"/>
-          <h1>AI Writing pro</h1>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, ratione aut nesciunt excepturi voluptatum eius.</p>
-          <p>
-            <span>$29</span>/month
-          </p>
-          <ul>
-            <li>
-              <FaCheck />
-              <p>Unlimited AI generations</p>
-            </li>
-            <li>
-              <FaCheck />
-              <p>Unlimited AI generations</p>
-            </li>
-            <li>
-              <FaCheck />
-              <p>Unlimited AI generations</p>
-            </li>
-          </ul>
-          <div className="mt-6">
-            <button className="btn btn-primary btn-block">Subscribe</button>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-full mx-auto">
+      {datas.map((data, index) => {
+        return (
+          <div key={index} className="card w-1/2 bg-base-200 shadow-md">
+            <div className="card-body relative space-y-2">
+              <p className="badge badge-xs badge-warning absolute top-3 right-3">{data.tag}</p>
+              <img src={portfolio} className="w-[25px]" />
+              <h1 className="text-2xl font-bold">AI Writing pro</h1>
+              <p className="text-slate-600 ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, ratione aut nesciunt excepturi voluptatum eius.</p>
+              <p className="text-slate-600 ">
+                <span className="text-black text-2xl font-semibold">$29</span>/month
+              </p>
+              <ul className="space-y-2">
+                <li className="flex items-center gap-2">
+                  <FaCheck className="text-green-500" />
+                  <p>Unlimited AI generations</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <FaCheck className="text-green-500" />
+                  <p>Unlimited AI generations</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <FaCheck className="text-green-500" />
+                  <p>Unlimited AI generations</p>
+                </li>
+              </ul>
+              <div className="mt-6">
+                <button className="btn btn-primary btn-block rounded-full">Subscribe</button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
--- src/components/Deals/Deals.jsx
+++ src/components/Deals/Deals.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { gradients } from "../../styles";
 import Card from "./Card";
 
+const fetchData = async() =>{
+  const res = await fetch("data.json")
+  return res.json();
+}
+
 function Deals() {
+  const dataPromise=fetchData();
   return (
     <div>
       <h1 className="text-5xl text-center font-bold mb-3">Premium Digital Tools</h1>
@@ -12,7 +18,9 @@ function Deals() {
         <button className={`px-4 py-2 ${gradients.primary} text-white font-bold rounded-full hover:cursor-pointer`}>Products</button>
         <button className="px-4 py-2 font-bold rounded-full border border-indigo-500 hover:cursor-pointer">Cart (0)</button>
       </div>
-      <Card></Card>
+      <Suspense fallback={<span className="loading loading-spinner text-primary"></span>}>
+        <Card dataPromise={dataPromise}></Card>
+      </Suspense>
     </div>
   );
 }
